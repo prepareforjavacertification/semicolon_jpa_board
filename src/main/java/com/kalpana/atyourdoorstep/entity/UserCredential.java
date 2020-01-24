5ed26784ddccb77f87ee7019dc8671d3b7424863
Manoj <prepareforjavacertification@gmail.com>
2020-01-24 17:18:52 +0530

    JPA one-to-one mapping example.

---
+++ src/main/java/com/kalpana/atyourdoorstep/entity/UserCredential.java
@@ -0,0 +1,50 @@
+package com.kalpana.atyourdoorstep.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER_CREDENTIAL")
+public class UserCredential implements Serializable {
+
+    @OneToOne
+    @JoinColumn(name = "USER_ID")
+    private UserDetails userDetails;
+
+    @Id
+    @Column(name = "USER_NAME")
+    private String userName;
+
+    @Column(name = "PASSWORD")
+    private String userPassword;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public UserDetails getUserDetails() {
+        return userDetails;
+    }
+
+    public void setUserDetails(UserDetails userDetails) {
+        this.userDetails = userDetails;
+    }
+
+}
